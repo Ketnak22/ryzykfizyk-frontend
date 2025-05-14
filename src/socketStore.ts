a84ff18182846df5430d032ddd81fe830bbc0474
Ketnak22 <salekantoni@gmail.com>
2025-05-14 16:21:04 +0200

    Naprawa błędu, który powodował, że tylko hostujący mógł łączyć się z serwerem.

--- src/socketStore.ts
+++ src/socketStore.ts
@@ -2,4 +2,4 @@ import { writable } from "svelte/store";
 
 import { io } from "socket.io-client";
 
-export const socket = writable(io("http://localhost:3001"))
+export const socket = writable(io(`${window.location.protocol}//${window.location.hostname}:3001`))
